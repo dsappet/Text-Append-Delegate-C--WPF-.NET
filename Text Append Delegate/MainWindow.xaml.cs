0ffc56f63722a4771bfdb3d2471b7a4b5ef41ea8
dsappet <dsappet@gmail.com>
2015-04-13 11:14:38 -0400

    C# Update window from another thread using delegates
    Initial commit of project.

---
+++ Text Append Delegate/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading; // required for dispatcher
+
+namespace Text_Append_Delegate
+{
+
+    public delegate void TextAppendDelegate(string text);
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextAppendDelegate appTxt;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        public void TextAppend(string text)
+        {
+            // invoke not included in wpf so we use dispatcher
+            // Note that CheckAccess() is hidden in intellisense!! 
+            if (this.TxtBox.Dispatcher.CheckAccess())
+            {
+                this.TxtBox.Text = text;
+            }
+            else
+            {
+                this.TxtBox.Dispatcher.BeginInvoke(new TextAppendDelegate(TextAppend), text);
+            }
+
+            // The following is an example of how to do the same thing in windows forms
+            //if (this.TxtBox.InvokeRequired)
+            //{
+            //    this.TxtBox.Invoke(TextAppend, text);
+            //}
+            //else this.TxtBox.Text = text;
+        }
+        public void RealAppend(string text)
+        {
+            this.TxtBox.AppendText(text);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            test myClass = new test(appTxt);
+            Thread myThread = new Thread(myClass.sendText);
+            myThread.Start("sample string");
+            if(myThread.IsAlive) myThread.Join();
+        }
+
+        private void Grid_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            appTxt = new TextAppendDelegate(TextAppend);
+
+        }
+    }
+
+}
---
+++ Text Append Delegate/test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Append_Delegate
+{
+    class test
+    {
+        private TextAppendDelegate mydel;
+        public test(TextAppendDelegate d){
+            mydel = d;
+        }
+        public void sendText(object txt)
+        {
+            mydel((string)txt);
+        }
+    }
+}
